2d7afcc0487c7ba5afa42bb6df86abc26b8ddec8
vigneshmuthu789 <292176184+vigneshmuthu789@users.noreply.github.com>
2026-09-17 13:58:30 +0530

    Update contact and social links in all footers (LinkedIn, Mail, Behance, WhatsApp)

--- src/sections/cognitive-tunneling/Frame17393293552.tsx
+++ src/sections/cognitive-tunneling/Frame17393293552.tsx
@@ -24,35 +24,23 @@ export default function Frame17393293552() {
         </div>
         <div className="vignesh-1khojdq" data-border="true" style={{ "--border-bottom-width": "0px", "--border-color": "rgba(51, 51, 51, 0.05)", "--border-left-width": "2px", "--border-right-width": "0px", "--border-style": "dashed", "--border-top-width": "2px" }}>
           <Suspense fallback={null}>
-            <a className="vignesh-g5ue9a vignesh-9xu8p4" data-border="true" href="https://in.linkedin.com/in/vignesh-muthu" target="_blank" rel="noopener" style={{ "--border-bottom-width": "2px", "--border-color": "rgba(51, 51, 51, 0.05)", "--border-left-width": "0px", "--border-right-width": "2px", "--border-style": "dashed", "--border-top-width": "0px" }}>
+            <a className="vignesh-g5ue9a vignesh-9xu8p4" data-border="true" href="https://www.linkedin.com/in/vigneshmuthua/" target="_blank" rel="noopener" style={{ "--border-bottom-width": "2px", "--border-color": "rgba(51, 51, 51, 0.05)", "--border-left-width": "0px", "--border-right-width": "2px", "--border-style": "dashed", "--border-top-width": "0px" }}>
               <div data-vignesh-component-type="SVG" data-vignesh-name="LinkedIn" parentsize="0" _constraints="[object Object]" rotation="0" shadows="" className="vignesh-pyiyun" aria-hidden="true" style={{ imageRendering: "pixelated", flexShrink: "0", backgroundSize: "100% 100%", backgroundImage: "url('data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 xmlns:xlink=%22http://www.w3.org/1999/xlink%22 viewBox=%220 0 28.59 27.002%22 overflow=%22visible%22><path d=%22M 6 3.002 C 6 3.797 5.683 4.56 5.12 5.122 C 4.557 5.685 3.794 6 2.999 6 C 2.203 6 1.44 5.683 0.878 5.12 C 0.315 4.557 0 3.794 0 2.999 C 0 2.203 0.317 1.44 0.88 0.878 C 1.443 0.315 2.206 0 3.002 0 C 3.797 0 4.56 0.317 5.122 0.88 C 5.685 1.443 6 2.206 6 3.002 Z M 6.09 8.222 L 0.09 8.222 L 0.09 27.002 L 6.09 27.002 Z M 15.57 8.222 L 9.6 8.222 L 9.6 27.002 L 15.51 27.002 L 15.51 17.147 C 15.51 11.657 22.665 11.147 22.665 17.147 L 22.665 27.002 L 28.59 27.002 L 28.59 15.107 C 28.59 5.852 18 6.197 15.51 10.742 Z%22 fill=%22rgba(102,102,102,0.4)%22></path></svg>')" }} />
             </a>
           </Suspense>
           <Suspense fallback={null}>
-            <a className="vignesh-1xetkzt vignesh-9xu8p4" data-border="true" href="https://www.instagram.com/_.VIGNESH MUTHU._design/" target="_blank" rel="noopener" style={{ "--border-bottom-width": "2px", "--border-color": "rgba(51, 51, 51, 0.05)", "--border-left-width": "0px", "--border-right-width": "0px", "--border-style": "dashed", "--border-top-width": "0px" }}>
-              <div data-vignesh-component-type="SVG" data-vignesh-name="Instagram" parentsize="0" _constraints="[object Object]" rotation="0" shadows="" className="vignesh-1ko914z" aria-hidden="true" style={{ imageRendering: "pixelated", flexShrink: "0" }}>
-                <div className="svgContainer" style={{ width: "100%", height: "100%", aspectRatio: "inherit" }}>
-                  <svg style={{ width: "100%", height: "100%", overflow: "visible" }}>
-                    <use href="#svg-1982340494_1221" />
-                  </svg>
-                </div>
-              </div>
+            <a className="vignesh-1xetkzt vignesh-9xu8p4" data-border="true" href="mailto:vigneshmuthu.arumugam@gmail.com" target="_blank" rel="noopener" style={{ "--border-bottom-width": "2px", "--border-color": "rgba(51, 51, 51, 0.05)", "--border-left-width": "0px", "--border-right-width": "0px", "--border-style": "dashed", "--border-top-width": "0px" }}>
+              <div data-vignesh-component-type="SVG" data-vignesh-name="Mail" parentsize="0" _constraints="[object Object]" rotation="0" shadows="" className="vignesh-1ko914z" aria-hidden="true" style={{ imageRendering: "pixelated", flexShrink: "0", backgroundSize: "100% 100%", backgroundImage: "url('data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 24 24%22 fill=%22none%22 stroke=%22rgba(102,102,102,0.4)%22 stroke-width=%222%22 stroke-linecap=%22round%22 stroke-linejoin=%22round%22><rect x=%222%22 y=%224%22 width=%2220%22 height=%2216%22 rx=%222%22/><path d=%22m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7%22/></svg>')" }} />
             </a>
           </Suspense>
           <Suspense fallback={null}>
-            <a className="vignesh-sedpem vignesh-9xu8p4" data-border="true" href="https://www.behance.net/VIGNESH MUTHU" target="_blank" rel="noopener" style={{ "--border-bottom-width": "0px", "--border-color": "rgba(51, 51, 51, 0.05)", "--border-left-width": "0px", "--border-right-width": "2px", "--border-style": "dashed", "--border-top-width": "0px" }}>
+            <a className="vignesh-sedpem vignesh-9xu8p4" data-border="true" href="https://www.behance.net/vigneshmut9292" target="_blank" rel="noopener" style={{ "--border-bottom-width": "0px", "--border-color": "rgba(51, 51, 51, 0.05)", "--border-left-width": "0px", "--border-right-width": "2px", "--border-style": "dashed", "--border-top-width": "0px" }}>
               <div data-vignesh-component-type="SVG" data-vignesh-name="Behance" parentsize="0" _constraints="[object Object]" rotation="0" shadows="" className="vignesh-1xwlp9k" aria-hidden="true" style={{ imageRendering: "pixelated", flexShrink: "0", backgroundSize: "100% 100%", backgroundImage: "url('data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 xmlns:xlink=%22http://www.w3.org/1999/xlink%22 viewBox=%220 0 36 22.5%22 overflow=%22visible%22><g><g><path d=%22M 33 3 L 22.5 3 L 22.5 0 L 33 0 Z M 35.589 18 C 34.926 19.945 32.546 22.5 27.937 22.5 C 23.326 22.5 19.591 19.906 19.591 13.987 C 19.591 8.122 23.079 5.107 27.79 5.107 C 32.414 5.107 35.236 7.781 35.853 11.747 C 35.97 12.506 36.016 13.528 35.995 14.956 L 23.955 14.956 C 24.15 19.773 29.179 19.924 30.837 18 Z M 24.06 12 L 31.507 12 C 31.35 9.679 29.803 8.672 27.792 8.672 C 25.593 8.672 24.376 9.823 24.06 12 Z M 9.699 22.482 L 0 22.482 L 0 0.031 L 10.43 0.031 C 18.644 0.153 18.799 8.198 14.51 10.391 C 19.701 12.281 19.875 22.482 9.699 22.482 Z M 4.5 9 L 9.876 9 C 13.638 9 14.235 4.5 9.408 4.5 L 4.5 4.5 Z M 9.587 13.5 L 4.5 13.5 L 4.5 18.024 L 9.511 18.024 C 14.094 18.024 13.814 13.5 9.587 13.5 Z%22 fill=%22rgba(102,102,102,0.4)%22></path></g></g></svg>')" }} />
             </a>
           </Suspense>
           <Suspense fallback={null}>
-            <a className="vignesh-13kai7p vignesh-9xu8p4" href="https://dribbble.com/ShRi_RaAm" target="_blank" rel="noopener">
-              <div data-vignesh-component-type="SVG" data-vignesh-name="Dribble" parentsize="0" _constraints="[object Object]" rotation="0" shadows="" className="vignesh-flyi1u" aria-hidden="true" style={{ imageRendering: "pixelated", flexShrink: "0" }}>
-                <div className="svgContainer" style={{ width: "100%", height: "100%", aspectRatio: "inherit" }}>
-                  <svg style={{ width: "100%", height: "100%", overflow: "visible" }}>
-                    <use href="#svg-1470600937_1018" />
-                  </svg>
-                </div>
-              </div>
+            <a className="vignesh-13kai7p vignesh-9xu8p4" href="https://wa.me/919344148837" target="_blank" rel="noopener">
+              <div data-vignesh-component-type="SVG" data-vignesh-name="WhatsApp" parentsize="0" _constraints="[object Object]" rotation="0" shadows="" className="vignesh-flyi1u" aria-hidden="true" style={{ imageRendering: "pixelated", flexShrink: "0", backgroundSize: "100% 100%", backgroundImage: "url('data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 24 24%22 fill=%22none%22 stroke=%22rgba(102,102,102,0.4)%22 stroke-width=%222%22 stroke-linecap=%22round%22 stroke-linejoin=%22round%22><path d=%22M3 21l1.65-3.8a9 9 0 1 1 3.4 2.9L3 21%22/><path d=%22M9 10a.5.5 0 0 0 1 0V9a.5.5 0 0 0-1 0v1a5 5 0 0 0 5 5h1a.5.5 0 0 0 0-1h-1a.5.5 0 0 0 0 1%22/></svg>')" }} />
             </a>
           </Suspense>
         </div>
